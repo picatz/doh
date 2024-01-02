267772fb69b965f7992ac1d1589bee7238674703
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2024-01-01 22:21:26 -0500

    Remove `core/*` packages

--- core/domain.go
+++
@@ -1,4 +0,0 @@
-package core
-
-// Domain is an alias for a string.
-type Domain = string
--- core/source.go
+++
@@ -1,9 +0,0 @@
-package core
-
-import "context"
-
-// Source defines the minimal interface for a DoH resolver.
-type Source interface {
-	Query(context.Context, Domain, Type) (*Response, error)
-	String() string
-}
